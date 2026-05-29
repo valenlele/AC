a34510b23314e9024c42daf75b0efc292a7ef5ff
valen <valentinocapuzzi789@gmail.com>
2026-05-29 19:23:18 -0300

    ej 4, 5 y 6 pract 3

--- Practica 4/ej6.s
+++ Practica 4/ej6.s
@@ -1,22 +1,34 @@
-.data
-  A: .word 5
-  B: .word 14
-  C: .word 26
-  D: .word 0
+  .data
+A: 
+  .word 5
+B:   
+  .word 1
+C:   
+  .word 5
+D:   
+  .word 0
 
-.code
+  .code
   ld r1, A(r0)
   ld r2, B(r0)
   ld r3, C(r0)
   daddi r4, r0, 0
 
   beq r1, r2, AyB
-  seguir:
-    
-  
-  AyB:
-      daddi r4, r4, 1
-  
-    
-  
+  beq r1, r3, dosIguales
+  beq r2, r3, dosIguales
+  j fin
+
+AyB: 
+  beq r1, r3, tresIguales
+
+dosIguales: 
+  daddi r4, r4, 2
+  j fin
+
+tresIguales:
+  daddi r4, r4, 3
+
+fin: 
+  sd r4, D(r0)
   halt
